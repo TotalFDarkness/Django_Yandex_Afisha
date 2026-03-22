56b9c036e7a6440501d667a705a9201e9961197b
TotalFDarkness <pavelmyagchilov@gmail.com>
2026-03-22 14:53:32 +0500

    add image review for admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.safestring import mark_safe
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    readonly_fields = ['image_preview']
+    fields = ['image', 'image_preview', 'position']
+    
+    def image_preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" width="200" />')
 
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
